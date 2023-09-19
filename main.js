c914e97fbca91651949713e90e70493c9973fe32
leonardomoreiradacruz <127850259+leonardomoreiradacruz@users.noreply.github.com>
2023-09-19 08:50:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
 function tocaSomPom(){
     document.querySelector('#som_telca_pom').play();
-document.querySelector(',tecla_pom').onclick = tocaSomPom
+document.querySelector(',tecla_pom').onclick = tocaSomPom;
 }
 function tocaSomClap(){
     document.querySelector('#som_telca_clap').play();
-document.querySelector(',tecla_clap').onclick = tocaSomClap
+document.querySelector(',tecla_clap').onclick = tocaSomClap;
 }
 function tocaSomTim(){
     document.querySelector('#som_telca_tim').play();
-document.querySelector(',tecla_tim').onclick = tocaSomTim
+document.querySelector(',tecla_tim').onclick = tocaSomTim;
 }
 function tocaSomPuff(){
     document.querySelector('#som_telca_puff').play();
-document.querySelector(',tecla_puff').onclick = tocaSomPuff
+document.querySelector(',tecla_puff').onclick = tocaSomPuff;
 }
 function tocaSomSplash(){
     document.querySelector('#som_telca_splash').play();
-document.querySelector(',tecla_splash').onclick = tocaSomSplash
+document.querySelector(',tecla_splash').onclick = tocaSomSplash;
 }
 function tocaSomToim(){
     document.querySelector('#som_telca_toim').play();
-document.querySelector(',tecla_toim').onclick = tocaSomToim
+document.querySelector(',tecla_toim').onclick = tocaSomToim;
 }
 function tocaSomPsh(){
     document.querySelector('#som_telca_psh').play();
-document.querySelector(',tecla_psh').onclick = tocaSomPsh
+document.querySelector(',tecla_psh').onclick = tocaSomPsh;
 }
 function tocaSomTic(){
     document.querySelector('#som_telca_tic').play();
-document.querySelector(',tecla_tic').onclick = tocaSomTic
+document.querySelector(',tecla_tic').onclick = tocaSomTic;
 }
 function tocaSomTom(){
     document.querySelector('#som_telca_tom').play();
-document.querySelector(',tecla_tom').onclick = tocaSomTom
+document.querySelector(',tecla_tom').onclick = tocaSomTom;
 }
+{
+ document.querySelectorA11('.tecla');}
