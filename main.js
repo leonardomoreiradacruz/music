2c13caa98e20328283cb0eb5ab4d9525ac330605
leonardomoreiradacruz <127850259+leonardomoreiradacruz@users.noreply.github.com>
2023-11-01 09:42:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento === null) {
+      alert('elemento não encontrado');
+    }
 
 }
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas=document.querySelectorAll('.tecla');
 
 
 // Estrutura de repetição - Enquanto
-while(  contador < listaDeTeclas.length){
+for( let contador = 0; contador < listaDeTeclas.length; contador++){
    const tecla = listaDeTeclas[contador]
    const instrumento = tecla.classList[1];
   const idAudiom = '#som_ ${instrumento}' ;
+     
+      console.log(idAudiom);
    
-     tecla.onclick = function(){
+   
+   tecla.onclick = function(){
         tocaSom(idAudiom);
      };
+      tecla.onkeydown = function(evento){
+         if(evento.code ==='Enter' || evento.code ==='Space'){
+         tecla.classList.add('ativa');
+         }
+      };
  tecla.onkeydown = function() {
     tecla.classList,add('ativa');
- }
+ };
  tecla.onkeydown = function(){
   tecla.classList.remove('ativa');
-}
-console.log(contador) ;
+};
+ console.log(instrumento);
+console.log(contador);
 
 }
